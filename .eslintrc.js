ec4dff3a4f399e46cdf1baaac7c028be86cac501
Josh Justice <josh.justice@accesscfa.com>
2019-11-18 08:59:02 -0500

    Add Detox ESLint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['detox'],
+  env: {
+    'detox/detox': true,
+  },
+  globals: {
+    __DEV__: true,
+    fail: true,
+  },
 };
