371f688cdcb2fa1d531e263f1257a834e422be44
Josh Justice <josh.justice@accesscfa.com>
2019-11-18 09:30:34 -0500

    Lay out add button next to text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {FlatList, SafeAreaView, StatusBar} from 'react-native';
+import {FlatList, SafeAreaView, StatusBar, View} from 'react-native';
 import {Button, Input, ListItem, ThemeProvider} from 'react-native-elements';
 
 const App: () => React$Node = () => {
@@ -16,12 +16,16 @@ const App: () => React$Node = () => {
     <ThemeProvider>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <Input
-          testID="New Todo Name field"
-          value={name}
-          onChangeText={setName}
-        />
-        <Button testID="Save Todo button" title="Save" onPress={handleSave} />
+        <View style={{flexDirection: 'row'}}>
+          <View style={{flex: 1}}>
+            <Input
+              testID="New Todo Name field"
+              value={name}
+              onChangeText={setName}
+            />
+          </View>
+          <Button testID="Save Todo button" title="Add" onPress={handleSave} />
+        </View>
         <FlatList
           data={todos}
           keyExtractor={item => item}
