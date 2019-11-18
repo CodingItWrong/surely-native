8b4bafaca42421cc0bdc93ae3095e3c62cb75eaa
Josh Justice <josh.justice@accesscfa.com>
2019-11-18 09:44:31 -0500

    Extract test function

--- e2e/managingTodos.spec.js
+++ e2e/managingTodos.spec.js
@@ -3,13 +3,17 @@ describe('managing todos', () => {
     await device.reloadReactNative();
   });
 
-  it('allows adding todos', async () => {
+  it('allows managing todos', async () => {
     const todoName = 'My First Todo';
 
+    await addTodo(todoName);
+  });
+
+  async function addTodo(todoName) {
     await element(by.id('New Todo Name field')).typeText(todoName);
     await element(by.id('Save Todo button')).tap();
 
     await expect(element(by.id('New Todo Name field'))).toHaveText('');
     await expect(element(by.text(todoName))).toBeVisible();
-  });
+  }
 });
