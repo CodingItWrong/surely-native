554a004c6f498820ec396eda117d1cf15a0d4bfd
Josh Justice <josh.justice@accesscfa.com>
2019-11-23 07:41:18 -0500

    Disable remote and backup based on env files

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import env from 'react-native-config';
 import MemorySource from '@orbit/memory';
 import JSONAPISource from '@orbit/jsonapi';
 import Coordinator, {RequestStrategy, SyncStrategy} from '@orbit/coordinator';
@@ -6,52 +7,54 @@ import schema from './schema';
 
 const memory = new MemorySource({schema});
 
-const remote = new JSONAPISource({
-  schema,
-  name: 'remote',
-  host: 'http://localhost:3000',
-});
+if (env.REMOTE_DATA === 'true') {
+  const remote = new JSONAPISource({
+    schema,
+    name: 'remote',
+    host: 'http://localhost:3000',
+  });
 
-const coordinator = new Coordinator({
-  sources: [memory, remote],
-});
+  const coordinator = new Coordinator({
+    sources: [memory, remote],
+  });
 
-// Query the remote server whenever the store is queried
-coordinator.addStrategy(
-  new RequestStrategy({
-    source: 'memory',
-    on: 'beforeQuery',
-    target: 'remote',
-    action: 'query',
-    blocking: true,
-  }),
-);
+  // Query the remote server whenever the store is queried
+  coordinator.addStrategy(
+    new RequestStrategy({
+      source: 'memory',
+      on: 'beforeQuery',
+      target: 'remote',
+      action: 'query',
+      blocking: true,
+    }),
+  );
 
-// Update the remote server whenever the store is updated
-coordinator.addStrategy(
-  new RequestStrategy({
-    source: 'memory',
-    on: 'beforeUpdate',
-    target: 'remote',
-    action: 'update',
-    blocking: false,
-  }),
-);
+  // Update the remote server whenever the store is updated
+  coordinator.addStrategy(
+    new RequestStrategy({
+      source: 'memory',
+      on: 'beforeUpdate',
+      target: 'remote',
+      action: 'update',
+      blocking: false,
+    }),
+  );
 
-// Sync all changes received from the remote server to the store
-coordinator.addStrategy(
-  new SyncStrategy({
-    source: 'remote',
-    target: 'memory',
-    blocking: false,
-  }),
-);
+  // Sync all changes received from the remote server to the store
+  coordinator.addStrategy(
+    new SyncStrategy({
+      source: 'remote',
+      target: 'memory',
+      blocking: false,
+    }),
+  );
 
-coordinator
-  .activate()
-  .then(() => {
-    console.log('Coordinator is active');
-  })
-  .catch(console.error);
+  coordinator
+    .activate()
+    .then(() => {
+      console.log('Coordinator is active');
+    })
+    .catch(console.error);
+}
 
 export default memory;
