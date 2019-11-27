1cd23936f2d03046ff86d3aa7d7c8f37666792b6
Josh Justice <josh.justice@accesscfa.com>
2019-11-27 06:56:33 -0500

    Store and load token on log in/out

--- src/Auth.js
+++ src/Auth.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, View} from 'react-native';
 import axios from 'axios';
 import {Button, TextInput} from 'react-native-paper';
 import {OAuth} from '@codingitwrong/react-login';
 import styles from './styles';
+import {loadToken, setToken, clearToken} from './store';
 
 const httpClient = axios.create({
   baseURL: 'http://localhost:3000',
 });
 
-const Auth = ({children}) => (
-  <OAuth
-    initiallyLoggedIn={false}
-    httpClient={httpClient}
-    renderForm={renderForm}
-    renderLoggedIn={({logOut}) => renderLoggedIn({children, logOut})}
-    handleAccessToken={token => console.log({token})}
-  />
-);
+const Auth = ({children}) => {
+  const [loading, setLoading] = useState(true);
+  const [initiallyLoggedIn, setInitiallyLoggedIn] = useState(false);
+
+  useEffect(() => {
+    loadToken().then(token => {
+      setInitiallyLoggedIn(!!token);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return null;
+  }
+
+  return (
+    <OAuth
+      initiallyLoggedIn={initiallyLoggedIn}
+      httpClient={httpClient}
+      renderForm={renderForm}
+      renderLoggedIn={({logOut}) => renderLoggedIn({children, logOut})}
+      handleAccessToken={setToken}
+    />
+  );
+};
 
 const renderForm = ({username, password, error, handleChange, handleLogIn}) => {
   return (
@@ -48,8 +65,13 @@ const renderForm = ({username, password, error, handleChange, handleLogIn}) => {
 const renderLoggedIn = ({children, logOut}) => (
   <View style={styles.fill}>
     {children}
-    <Button onPress={logOut}>Sign Out</Button>
+    <Button onPress={handleLogOut(logOut)}>Sign Out</Button>
   </View>
 );
+
+const handleLogOut = logOut => () =>
+  clearToken()
+    .then(logOut)
+    .catch(console.error);
 
 export default Auth;
