7e3424c0a0de0bedc4436f1cb8fe0a934e177e86
Josh Justice <josh.justice@accesscfa.com>
2019-11-28 11:49:53 -0500

    Add divider

--- src/TodoContainer/TodoListItem.js
+++ src/TodoContainer/TodoListItem.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import {View} from 'react-native';
-import {List} from 'react-native-paper';
+import {Divider, List} from 'react-native-paper';
 import CompleteButton from './CompleteButton';
 import DeleteButton from './DeleteButton';
 
 const TodoListItem = ({item, onComplete, onDelete}) => {
   const {name} = item.attributes;
   return (
-    <List.Item
-      title={name}
-      right={() => (
-        <View style={styles.buttonGroup}>
-          <CompleteButton
-            style={styles.button}
-            testID={`Complete todo ${name} button`}
-            onPress={() => onComplete(item)}
-          />
-          <DeleteButton
-            style={styles.button}
-            testID={`Delete todo ${name} button`}
-            onPress={() => onDelete(item)}
-          />
-        </View>
-      )}
-    />
+    <>
+      <List.Item
+        title={name}
+        right={() => (
+          <View style={styles.buttonGroup}>
+            <CompleteButton
+              style={styles.button}
+              testID={`Complete todo ${name} button`}
+              onPress={() => onComplete(item)}
+            />
+            <DeleteButton
+              style={styles.button}
+              testID={`Delete todo ${name} button`}
+              onPress={() => onDelete(item)}
+            />
+          </View>
+        )}
+      />
+      <Divider />
+    </>
   );
 };
 
