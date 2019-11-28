cc7ddb3e0375a905acf447fe8df3bad8b671c314
Josh Justice <josh.justice@accesscfa.com>
2019-11-28 12:27:32 -0500

    Apply green theme

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import {DefaultTheme} from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#4caf50',
+  },
+};
+
+export default theme;
