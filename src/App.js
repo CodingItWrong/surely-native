5b183eedd4d6c4ed5fbbc0a7dcfed2c93257b0c2
Josh Justice <josh.justice@accesscfa.com>
2019-12-15 06:46:25 -0500

    Add custom drawer items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
-import {Provider as PaperProvider} from 'react-native-paper';
+import {Provider as PaperProvider, Drawer} from 'react-native-paper';
 import {createAppContainer} from 'react-navigation';
 import {createDrawerNavigator} from 'react-navigation-drawer';
 import Auth from './Auth';
@@ -8,9 +8,18 @@ import TodoScreen from './screens/TodoScreen';
 import theme from './theme';
 import styles from './styles';
 
-const MyDrawerNavigator = createDrawerNavigator({
-  TodoScreen,
-});
+const MyDrawerNavigator = createDrawerNavigator(
+  {
+    TodoScreen,
+  },
+  {
+    contentComponent: () => (
+      <>
+        <Drawer.Item label="Log Out" />
+      </>
+    ),
+  },
+);
 
 const MyApp = createAppContainer(MyDrawerNavigator);
 
