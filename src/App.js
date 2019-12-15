0eba676ef47853c847d8faa8c9426b84da3a9037
Josh Justice <josh.justice@accesscfa.com>
2019-12-15 06:41:21 -0500

    Move app into drawer navigator

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
+import {createAppContainer} from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer';
 import Auth from './Auth';
 import TodoScreen from './screens/TodoScreen';
 import theme from './theme';
 import styles from './styles';
+
+const MyDrawerNavigator = createDrawerNavigator({
+  TodoScreen,
+});
+
+const MyApp = createAppContainer(MyDrawerNavigator);
 
 const App = () => {
   return (
     <PaperProvider theme={theme}>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.fill}>
-        <Auth render={({logOut}) => <TodoScreen logOut={logOut} />} />
+        <Auth render={({logOut}) => <MyApp />} />
       </SafeAreaView>
     </PaperProvider>
   );
