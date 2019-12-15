95070cfe95f4b92d88989792c3b4c743c97bf78b
Josh Justice <josh.justice@accesscfa.com>
2019-12-15 06:55:08 -0500

    Extract DrawerNav component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
-import {Provider as PaperProvider, Drawer} from 'react-native-paper';
-import {createAppContainer} from 'react-navigation';
-import {createDrawerNavigator} from 'react-navigation-drawer';
+import {Provider as PaperProvider} from 'react-native-paper';
 import Auth from './Auth';
-import TodoScreen from './screens/TodoScreen';
+import DrawerNav from './DrawerNav';
 import theme from './theme';
 import styles from './styles';
-
-const DrawerNav = ({logOut}) => {
-  const MyDrawerNavigator = createDrawerNavigator(
-    {
-      TodoScreen,
-    },
-    {
-      contentComponent: () => (
-        <>
-          <Drawer.Item
-            label="Log Out"
-            onPress={() => {
-              console.log('logging out', logOut);
-              // logOut();
-            }}
-          />
-        </>
-      ),
-    },
-  );
-
-  const AppContainer = createAppContainer(MyDrawerNavigator);
-  return <AppContainer />;
-};
 
 const App = () => {
   return (
