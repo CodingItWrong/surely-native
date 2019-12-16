ebf6ec5ad8b89137da5a2cbcc1e60652840084ac
Josh Justice <josh.justice@accesscfa.com>
2019-12-16 06:12:19 -0500

    Clear existing records

--- src/useOrbitQuery.js
+++ src/useOrbitQuery.js
@@ -3,7 +3,12 @@ import {useState, useEffect, useCallback} from 'react';
 const useOrbitQuery = ({storeReady, store, query}) => {
   const [records, setRecords] = useState([]);
 
-  const refresh = useCallback(() => store.query(query), [store, query]);
+  const refresh = useCallback(() => {
+    // this is model specific and needs to be removed
+    const allRecords = store.cache.query(q => q.findRecords('todo'));
+    store.cache.patch(t => allRecords.map(r => t.removeRecord(r)));
+    store.query(query);
+  }, [store, query]);
 
   useEffect(() => {
     storeReady().then(() => {
