c7e4c71828849bb5f92d4dd5ad50a3b556a90f9c
Josh Justice <josh.justice@accesscfa.com>
2019-12-30 09:33:01 -0500

    Style modal better

--- src/screens/TodoScreen/TodoDetailModal.js
+++ src/screens/TodoScreen/TodoDetailModal.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import {Button, Modal, Text} from 'react-native';
+import {Modal, SafeAreaView} from 'react-native';
+import {Appbar, Button, Divider, List} from 'react-native-paper';
 
 const TodoDetailModal = ({todo, onDismiss, onDelete}) => {
   return (
     <Modal visible={todo !== null}>
-      <Text>{todo && todo.name}</Text>
-      <Button
-        testID="Delete todo button"
-        title="Delete"
-        onPress={() => onDelete(todo)}
-      />
-      <Button title="Close" onPress={onDismiss} />
+      <SafeAreaView>
+        <Appbar>
+          <Appbar.Content title="Edit Todo" />
+          <Appbar.Action icon="close" onPress={onDismiss} />
+        </Appbar>
+        <List.Item title={todo && todo.name} titleNumberOfLines={3} />
+        <Divider />
+        <Button
+          testID="Delete todo button"
+          title="Delete"
+          color="red"
+          onPress={() => onDelete(todo)}>
+          Delete
+        </Button>
+      </SafeAreaView>
     </Modal>
   );
 };
